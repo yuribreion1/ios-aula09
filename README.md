6ff368733e7021d790b1e6c8261def5dd44e2127
Usuário Convidado <Guest@l2602micro09.local>
2019-04-10 20:04:47 -0300

    Aula 09, usando Slider, Spinner e Segmented Control

---
+++ README.md
@@ -0,0 +1,8 @@
+#  Aula 09 - Swift
+
+- Criamos um projeto _Swift_
+- Clicar em _Main.storyboard_
+- Vamos criar um _segmented control_
+- Criamos algumas _labels_
+- Para alterar o _segmented control_  usamos a aba no canto direito
+- Definimos valores para o _Stepper_ e para o _Slider_ 
--- aula-09/ViewController.swift
+++ aula-09/ViewController.swift
@@ -9,12 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var meuSegmento: UISegmentedControl!
+    @IBOutlet weak var lblAno: UILabel!
+    @IBOutlet weak var lblKm: UILabel!
+    @IBOutlet weak var lblRota: UILabel!
+    @IBOutlet weak var meuStepper: UIStepper!
+    
+    @IBAction func indexChanged(_ sender: Any) {
+        switch (meuSegmento.selectedSegmentIndex) {
+        case 0:
+            print("Você escolheu o \(meuSegmento.titleForSegment(at: 0)!)");
+            break;
+        case 1:
+            print("Você escolheu o \(meuSegmento.titleForSegment(at: 1)!)");
+            break;
+        case 2:
+            print("Você escolheu o \(meuSegmento.titleForSegment(at: 2)!)");
+        default:
+            <#code#>
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        lblAno.text = "1980";
+        lblKm.text = "7500";
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func salvar(_ sender: Any) {
+        
+        // Formato explicito de criacao de variavel
+        var numero1:Int = 8
+        // Formato explicito declarativo
+        var total = numero1 + 40
+        print(total)
+        
+        //Quando vc cria uma variavel com ? ela é chamada de _Optional_
+        
+        var numero2:Int?
+        numero2 = nil //Nulo
+        numero2 = 10
+        total = numero2! + 20 //! é usado para fazer o unwrapper do conteúdo
+        print(total)
+        
+        var nome:String="Maria Souza"
+        var nome2:String?
+        nome2="Joana Cruz"
+        var nome3:String!
+        nome3="Mario Lago"
+        print(nome);
+        print(nome2);
+        
+        //Para imprimir de forma explicita
+        print(nome2!);
+        print(nome3);
+        
+    }
+    
 }
 
